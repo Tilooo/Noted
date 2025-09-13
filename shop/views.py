bc26933327bd5fecdf6780ced803cd936da107a8
Tilooo <moonaviad@gmail.com>
2025-09-13 22:48:29 +0300

    5. URL for the Detail Page. A new Python function that gets a single product from the database based on its ID.

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -10,3 +10,13 @@ def product_list(request):
     }
 
     return render(request, 'shop/product_list.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    context = {
+        'product': product
+    }
+
+    return render(request, 'shop/product_detail.html', context)
