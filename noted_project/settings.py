3b9a0740570849d9ff724e5bc97ea44ceac27d4b
Tilooo <moonaviad@gmail.com>
2025-09-14 14:40:53 +0300

    7. Cart App, and the Cart Logic.

--- noted_project/settings.py
+++ noted_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'shop',
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,5 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CART_SESSION_ID = 'cart'
