370bd764553914692c4d567b6123929f5fb2f1f6
Tilooo <moonaviad@gmail.com>
2025-09-15 14:43:10 +0300

    8. The Cart Detail Page Template, and its Styles.

---
+++ cart/views.py
@@ -0,0 +1,17 @@
+# cart/views.py
+
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product, quantity=1)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_POST
-from shop.models import Product
-from .cart import Cart
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def product_list(request):
@@ -22,15 +20,3 @@ def product_detail(request, pk):
     }
 
     return render(request, 'shop/product_detail.html', context)
-
-
-@require_POST  # only POST requests can access this view
-def cart_add(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.add(product=product, quantity=1)
-    return redirect('cart:cart_detail')
-
-def cart_detail(request):
-    cart = Cart(request)
-    return render(request, 'cart/detail.html', {'cart': cart})
