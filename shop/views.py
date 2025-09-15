4bc5ec7b1e76bcb34e3051d7cfaddfcaa0d78885
Tilooo <moonaviad@gmail.com>
2025-09-15 14:24:26 +0300

    7. The view to handle the logic when a user clicks the "Add to Cart" button..

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
 
 
 def product_list(request):
@@ -20,3 +22,15 @@ def product_detail(request, pk):
     }
 
     return render(request, 'shop/product_detail.html', context)
+
+
+@require_POST  # only POST requests can access this view
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.add(product=product, quantity=1)
+    return redirect('cart:cart_detail')
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
