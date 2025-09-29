18800f860a1d9400746fe7e681e45cdb3e75ff44
Tilooo <moonaviad@gmail.com>
2025-09-29 17:18:14 +0300

    31. Checkout with Tailwind.

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,19 @@
+# orders/forms.py
+
 from django import forms
 from .models import Order
+
+tailwind_input_classes = 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
 
 class OrderCreateForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['first_name', 'last_name', 'email', 'address', 'postal_code', 'city']
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': tailwind_input_classes, 'placeholder': 'John'}),
+            'last_name': forms.TextInput(attrs={'class': tailwind_input_classes, 'placeholder': 'Love'}),
+            'email': forms.EmailInput(attrs={'class': tailwind_input_classes, 'placeholder': 'you@example.com'}),
+            'address': forms.TextInput(attrs={'class': tailwind_input_classes, 'placeholder': '1234 Main St'}),
+            'postal_code': forms.TextInput(attrs={'class': tailwind_input_classes, 'placeholder': '10001'}),
+            'city': forms.TextInput(attrs={'class': tailwind_input_classes, 'placeholder': 'New York'}),
+        }
